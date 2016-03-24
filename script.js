ec25b354bc9dd139e9d6c21521061b4e30003f47
Gunther Schneider <pixeloon@gmail.com>
2016-03-24 15:31:38 -0700

    black and red

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+// // Your JS goes here
+// Each tile should be a div
+// Each tile's width is 11.1%
+// Set each tile's float property to left
+// Each tile's paddingBottom is 11.1%
+
+var body = document.querySelector("body");
+var tile;
+body.style.margin = 0;
+
+for (i = 1; i <= 63; i++) {
+    tile = document.createElement("div");
+
+    tile.style.float = ("left");
+    tile.style.paddingBottom = ("11.1%");
+    tile.style.width = ("11.1%");
+
+    if (i % 2 === 0) {
+        tile.style.backgroundColor = ("#000");
+    } else {
+        tile.style.backgroundColor = ("#FF0000");
+    }
+    body.appendChild(tile);
+}
